234b97a58ed72fff141b758aa31fb4060688e543
ajsmorillas <antonio@imorillas.com>
2026-06-14 10:43:21 +0200

    fix varios 157

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   // AÑADE ESTA LÍNEA (sustituye por tu dominio real)
   site: 'https://hotelads.es',
-  
+
+  integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
     build: {
-      assetsInlineLimit: 10240 
+      assetsInlineLimit: 10240
     }
   }
 });
